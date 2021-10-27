65dd8c118ed79a1273f8b37def4a914e805eabc9
Natalia Canudas <natalia.200351@hotmail.com>
2021-10-27 12:32:39 +0200

    OCT 27

---
+++ Ejercicio07/app/src/main/java/com/example/ejercicio07/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ejercicio07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        crearMenu();
+    }
+
+    public void crearMenu() {
+        Menu menu;
+    }
+}
