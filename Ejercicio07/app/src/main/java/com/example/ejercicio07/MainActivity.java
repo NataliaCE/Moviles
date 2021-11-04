0b362fa8e3a0333592c304afa7f54da95f5b002b
Natalia Canudas <natalia.200351@hotmail.com>
2021-11-04 14:18:48 +0100

    NOV 04

--- Ejercicio07/app/src/main/java/com/example/ejercicio07/MainActivity.java
+++ Ejercicio07/app/src/main/java/com/example/ejercicio07/MainActivity.java
@@ -1,26 +1,43 @@
+/**
+ * Ejercicio 07 - Menús
+ *
+ * @author Natalia Canudas
+ */
 package com.example.ejercicio07;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Se le llama cuando la actividad está comenzando
+     *
+     * @param savedInstanceState - Contiene los datos de la actividad si se ha cerrado previamente.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Inicializa el contenido del menú de opciones
+     *
+     * @param menu - Menú en el que se van a colocar los elementos
+     * @return true - Siempre devuelve verdadero
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        Menu ajustes = menu.addSubMenu(R.string.ajustes);
-        ajustes.add(R.string.color);
-        ajustes.add(R.string.letra);
-        menu.add(R.string.informacion);
+        SubMenu ajustes = menu.addSubMenu(R.string.ajustes);
+        MenuItem color = ajustes.add(R.string.color);
+        MenuItem letra = ajustes.add(R.string.letra);
+        MenuItem información = menu.add(R.string.informacion);
 
         return true;
     }
+
 }
